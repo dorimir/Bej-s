0c06d10d18af15f721dae1bbb7ef38059ad62cd0
frosales04 <al426022@uji.es>
2025-11-05 19:39:08 +0100

    minijuego de pesca

--- BejisTriple/Assets/Scripts/Pesca/Anzuelo.cs
+++ BejisTriple/Assets/Scripts/Pesca/Anzuelo.cs
@@ -6,6 +6,10 @@ public class Anzuelo : MonoBehaviour
     float verticalInput;
     public float speed = 1;
     Rigidbody body;
+    //Si llevamos un pez a la superficie, lo quitamos del anzuelo y podemos seguir pescando
+    public GameObject surface;
+
+    bool HasSomethingHooked = false;
 
     void Start()
     {
@@ -17,5 +21,26 @@ public class Anzuelo : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
         body.linearVelocity = new Vector2(horizontalInput * speed, verticalInput * speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == surface)
+        {
+            Destroy(transform.GetChild(1).gameObject);
+            HasSomethingHooked = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (HasSomethingHooked == true) return;
+        if(collision.tag == "Trucha" || collision.tag == "Carpa" || collision.tag == "Siluro" || collision.tag == "Barbo" || collision.tag == "BasuraDeRio")
+        {
+            collision.transform.SetParent(this.transform);
+            collision.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePosition;
+            collision.GetComponent<Rigidbody>().linearVelocity = new UnityEngine.Vector2(0, 0);
+            HasSomethingHooked = true;
+        }
     }
 }
--- BejisTriple/Assets/Scripts/Pesca/FishSpawner.cs
+++ BejisTriple/Assets/Scripts/Pesca/FishSpawner.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 public class FishSpawner : MonoBehaviour
 {
-    public GameObject carpa, trucha, siluro, barbo;
+    public GameObject carpa, trucha, siluro, barbo, basura, bubbleParticles;
+    public float speed = 3;
 
     /*
     
     Cada segundo aparece un nuevo pez.
-    9/20 veces aparece una trucha (45%)
-    7/20 veces aparece una carpa (35%)
+    8/20 veces aparece una trucha (40%)
+    8/20 veces aparece una carpa (40%)
     3/20 veces aparece un siluro (15%)
     1/20 veces aparece un barbo dorado (5%)
     
@@ -19,46 +20,123 @@ public class FishSpawner : MonoBehaviour
     void Start()
     {
         StartCoroutine(SpawnFish());
+        StartCoroutine(SpawnTrash());
     }
 
     private IEnumerator SpawnFish()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.6f);
         var rand = new System.Random();
-        double randomNumber = rand.NextDouble();
         GameObject spawnedFish;
-        float spawnPointY = (float)((rand.NextDouble() * 6) + 6);
+
+        //Se decide a qué profundidad aparecerá el pez
+        float spawnPointY = (float)((rand.NextDouble() * 5.5f) + 6.4f);
+
+        //Se decide si aparecerá a la izquierda o a la derecha
         double Xchooser = rand.NextDouble();
         float spawnPointX;
         if (Xchooser >= 0.5) spawnPointX = 10;
         else spawnPointX = -10;
+
+        //Se decide qué pez será
+        double randomNumber = rand.NextDouble();
         if (randomNumber <= 0.05)
         {
             //Spawn barbo dorado
-            spawnedFish = Instantiate(barbo, new UnityEngine.Vector3(spawnPointX, spawnPointY, 2), UnityEngine.Quaternion.identity);
+
+            //Mostrar las partículas de burbujas
+            if (spawnPointX < 0)
+            {
+                GameObject burbujas = Instantiate(bubbleParticles, new UnityEngine.Vector3(-8.5f, spawnPointY, 2), UnityEngine.Quaternion.identity);
+                burbujas.transform.Rotate(0, 90, 0);
+            }
+            else
+            {
+                Instantiate(bubbleParticles, new UnityEngine.Vector3(8.5f, spawnPointY, 2), UnityEngine.Quaternion.identity);
+            }
+            StartCoroutine(SpawnBarbo(spawnPointX, spawnPointY));
+            yield break;
         }
         else if (randomNumber <= 0.15)
         {
             //Spawn siluro
             spawnedFish = Instantiate(siluro, new UnityEngine.Vector3(spawnPointX, spawnPointY, 2), UnityEngine.Quaternion.identity);
+            speed = 5;
         }
-        else if (randomNumber <= 0.35)
+        else if (randomNumber <= 0.40)
         {
             //Spawn carpa
             spawnedFish = Instantiate(carpa, new UnityEngine.Vector3(spawnPointX, spawnPointY, 2), UnityEngine.Quaternion.identity);
+            speed = 5;
         }
         else
         {
+            //Spawn trucha
             spawnedFish = Instantiate(trucha, new UnityEngine.Vector3(spawnPointX, spawnPointY, 2), UnityEngine.Quaternion.identity);
+            speed = 3;
         }
-        spawnedFish.GetComponent<Rigidbody>().linearVelocity = new UnityEngine.Vector2(7 * (-spawnPointX/10), spawnedFish.GetComponent<Rigidbody>().linearVelocity.y);
-        StartCoroutine(DeleteFish(spawnedFish));
+
+        //Si el pez empieza desde la izquierda, mirará a la derecha
+        if (spawnPointX < 0) spawnedFish.GetComponent<Transform>().localScale = new UnityEngine.Vector3(-0.52f, 0.52f, 0.52f);
+
+        //Velocidad del pez
+        spawnedFish.GetComponent<Rigidbody>().linearVelocity = new UnityEngine.Vector2(speed * (-spawnPointX / 10), spawnedFish.GetComponent<Rigidbody>().linearVelocity.y);
+
+        StartCoroutine(DeleteFish(spawnedFish, 7f));
         StartCoroutine(SpawnFish());
     }
-
-    private IEnumerator DeleteFish(GameObject fish)
+    
+    private IEnumerator SpawnBarbo(float spawnPointX, float spawnPointY)
     {
-        yield return new WaitForSeconds(10f);
-        fish.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        GameObject spawnedFish = Instantiate(barbo, new UnityEngine.Vector3(spawnPointX, spawnPointY, 2), UnityEngine.Quaternion.identity);
+        speed = 10;
+
+        //Si el pez empieza desde la izquierda, mirará a la derecha
+        if (spawnPointX < 0) spawnedFish.GetComponent<Transform>().localScale = new UnityEngine.Vector3(-0.52f, 0.52f, 0.52f);
+
+        //Velocidad del pez
+        spawnedFish.GetComponent<Rigidbody>().linearVelocity = new UnityEngine.Vector2(speed * (-spawnPointX / 10), spawnedFish.GetComponent<Rigidbody>().linearVelocity.y);
+
+        StartCoroutine(DeleteFish(spawnedFish, 7f));
+        StartCoroutine(SpawnFish());
+    }
+    
+    private IEnumerator SpawnTrash()
+    {
+        yield return new WaitForSeconds(0.8f);
+        var rand = new System.Random();
+        //Profundidad a la que aparecerá la basura
+        float spawnPointY = (float)((rand.NextDouble() * 5) + 6.4f);
+        //Si aparecerá a la izquierda o derecha
+        double Xchooser = rand.NextDouble();
+        float spawnPointX;
+        if (Xchooser >= 0.5) spawnPointX = 10;
+        else spawnPointX = -10;
+        //Creamos la basura
+        GameObject spawnedTrash = Instantiate(basura, new UnityEngine.Vector3(spawnPointX, spawnPointY, 2), UnityEngine.Quaternion.identity);
+
+        //Decidimos si será una herradura rota, un champiñón o un hueso
+        double randomNumber = rand.NextDouble();
+        if (randomNumber <= 0.33f)
+        {
+            spawnedTrash.GetComponent<Animator>().SetBool("champinon", true);
+        }
+        else if (randomNumber <= 0.67f)
+        {
+            spawnedTrash.GetComponent<Animator>().SetBool("hueso", true);
+        }
+
+        //Velocidad
+        spawnedTrash.GetComponent<Rigidbody>().linearVelocity = new UnityEngine.Vector2(3 * (-spawnPointX / 10), spawnedTrash.GetComponent<Rigidbody>().linearVelocity.y);
+        
+        StartCoroutine(DeleteFish(spawnedTrash, 7f));
+        StartCoroutine(SpawnTrash());
+    }
+
+    private IEnumerator DeleteFish(GameObject fish, float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(fish);
     }
 }
