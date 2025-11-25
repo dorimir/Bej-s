c1a90642ab3f58e4c0d9d0a44d918d89a5796d05
frosales04 <al426022@uji.es>
2025-11-25 23:04:04 +0100

    no mucho en realidad

---
+++ BejisTriple/Assets/Scripts/Arabe_activarOpciones.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class Arabe_activarOpciones : activarOpciones
+{
+    protected override void Update()
+    {
+        if (jugadorDentro && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (manager != null && manager.dialogoActivo) 
+            {
+                Debug.Log("No se pueden mostrar opciones: diálogo activo.");
+                return;
+            }
+
+            MostrarOpciones();
+        }
+    }
+
+}
