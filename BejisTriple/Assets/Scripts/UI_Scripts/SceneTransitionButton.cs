09a21cd98d7b60741b9915d9fc6c032f39970b77
dorimir <144936659+dorimir@users.noreply.github.com>
2025-11-29 12:19:31 +0100

    Prefab_transicionesEscenaws

---
+++ BejisTriple/Assets/Scripts/UI_Scripts/SceneTransitionButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager Instance;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private float transitionTime;
+    [SerializeField] private int nextSceneIndex;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(Transition(nextSceneIndex));
+    }
+
+    public IEnumerator Transition(int sceneIndex)
+    {
+        animator.SetTrigger("End");
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneIndex);
+
+        animator.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+    }
+}
