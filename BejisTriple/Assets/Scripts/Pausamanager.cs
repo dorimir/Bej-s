d10652102e4f68fbfc0e61fdcdc1ca24eb22492c
dorimir <144936659+dorimir@users.noreply.github.com>
2025-12-07 17:52:27 +0100

    BotóndePausa_MenúDePausa

--- BejisTriple/Assets/Scripts/Pausamanager.cs
+++ BejisTriple/Assets/Scripts/Pausamanager.cs
@@ -1,57 +1,86 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Pausamanager : MonoBehaviour
 {
+    [SerializeField] private Button pauseButton;   // <-- botón asignable desde inspector
 
     public GameObject Menupausa;
     public GameObject menuconfirmacion;
+
     private bool pausado = false;
+    private bool pausandoAhora = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (pauseButton != null)
+            pauseButton.onClick.AddListener(() =>
+            {
+                if (pausado) Despausar();
+                else Pausar();
+            });
+    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (pausado) despausar();
-            else pausa();
+            if (pausado) Despausar();
+            else Pausar();
         }
     }
 
-    public void pausa(){
+    public void Pausar()
+    {
+        if (pausandoAhora) return;
+
         Menupausa.SetActive(true);
+        pausandoAhora = true;
+
+        Invoke(nameof(ActivarTimeScaleCero), 0.2f);
+    }
+
+    public void TogglePause()
+    {
+        if (pausado) Despausar();
+        else Pausar();
+    }
+
+    private void ActivarTimeScaleCero()
+    {
         Time.timeScale = 0f;
-        pausado=true;
+        pausado = true;
+        pausandoAhora = false;
     }
 
-    public void despausar(){
-        Menupausa.SetActive(false);
+    public void Despausar()
+    {
         Time.timeScale = 1f;
-        pausado=false;
+        Menupausa.SetActive(false);
+        pausado = false;
     }
 
-
-
-    public void Salir(){
+    public void Salir()
+    {
         Menupausa.SetActive(false);
         menuconfirmacion.SetActive(true);
-
     }
 
-    public void confirmarSalir(){
+    public void confirmarSalir()
+    {
         Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
 
-    public void NoSalirMenu(){
+    public void NoSalirMenu()
+    {
         menuconfirmacion.SetActive(false);
         Menupausa.SetActive(true);
-
     }
 
-    public void continuar(){
-        despausar();
+    public void continuar()
+    {
+        Despausar();
     }
 }
